6ef4de1d66ba66805f88579ee4979b5a66c15d34
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-17 10:56:02 -0600

    Fix linters

--- spec/views/foods/show.html.erb_spec.rb
+++ spec/views/foods/show.html.erb_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "foods/show", type: :view do
+RSpec.describe 'foods/show', type: :view do
   before(:each) do
     assign(:food, Food.create!(
-      name: "Name",
-      measurement_unit: 2,
-      price: 3,
-      quantity: 4
-    ))
+                    name: 'Name',
+                    measurement_unit: 2,
+                    price: 3,
+                    quantity: 4
+                  ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/2/)
