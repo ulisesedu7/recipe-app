3ec872d165daa584bc0120582c78a8ff82c3dcfb
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-19 12:41:29 -0600

    Add styles to the navbar and modify the root for the shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Root for the Shoping list
+  get get '/shopping_list', to: 'foods#shop'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
