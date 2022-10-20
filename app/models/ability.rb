ff71019560e6b1b17874c8ce52438f4a95d6767b
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-10-20 18:23:51 -0400

    Fix linters errors 1

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -35,7 +33,6 @@ class Ability
     return unless user.present?
 
     can :manage, Food, user: user
-    can :manage, Recipe, user: user
-
+    can :manage, Recipe, user:
   end
 end
