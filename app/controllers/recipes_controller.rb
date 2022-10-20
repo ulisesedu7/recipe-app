25a02b6d39531b239d7bb55b0d34d014b28a6f53
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-19 21:23:20 -0600

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,7 +27,7 @@ class RecipesController < ApplicationController
   # POST /recipes or /recipes.json
   def create
     @user = current_user
-    @recipe =  @current_user.recipes.create(recipe_params)
+    @recipe = @current_user.recipes.create(recipe_params)
 
     respond_to do |format|
       if @recipe.save
